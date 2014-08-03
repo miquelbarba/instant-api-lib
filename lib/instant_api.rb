868eebc97cfa09a04077a3e43c6c3b0452cf68be
Miquel Barba <miquel.barba@gmail.com>
2014-08-03 21:52:58 +0200

    Fix uninitialized constant

--- lib/instant_api.rb
+++ lib/instant_api.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 
+module InstantApi
+end
+
 require 'kaminari'
 require 'instant_api/util/array'
 require 'instant_api/controller/routes'
 
 # why?
 Kaminari::Hooks.init
-
-module InstantApi
-end
 
 
